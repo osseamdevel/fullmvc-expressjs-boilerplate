cfd8b31a7840f182f22243ed612b4720842003cb
osseam <60955724+osseamdevel@users.noreply.github.com>
2024-06-01 09:19:53 +0100

    Update middleware.js

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -3,7 +3,15 @@ exports.middlewareGlobal = (request, response, next) => {
   next();
 };
 
+exports.csrfErrorHandler = (error, request, response, next) => {
+  if (error.code === 'EBADCSRFTOKEN') {
+    return response.status(403).render('404', { message: 'Erro CSRF detectado!' });
+  } else {
+    return response.status(500).render('404', { message: 'Erro interno do servidor' });
+  }
+};
+
 exports.checkCsrf = (request, response, next) => {
   response.locals.csrfToken = request.csrfToken();
   next();
-}
+};
